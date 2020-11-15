719392069f189ebddb6e8990ea76c53fc5b4a2ed
sem sabiduria <semsabiduria@gmail.comgit config --global user.email semsabiduria@gmail.comgit config --global user.email semsabiduria@gmail.com>
2020-11-15 21:25:12 +0800

    Add Installer

--- tos.sh
+++ tos.sh
@@ -80,13 +80,13 @@ echo 'termux-media-player play "$track"' >>playl.sh
 echo 'id1=$(<id)' >>playl.sh
 #echo 'termux-notification --action "mediaplay 'serve' " --type media --media-previous "termux-media-player pause; echo yes>prev " --media-play "termux-media-player 'play'" --media-pause "termux-media-player 'pause' " --media-next "termux-media-player stop"  -t "🎧$pinfo" --content "$choice" --sound --vibrate 800 --priority high  --image-path "$HOME/test.png" --id "$id1" --on-delete "rm -rf  tmp"  ' >>playl.sh
 #echo 'mediaplay serve'>>playl.sh
-echo 'showplay'>>playl.sh
+echo 'bash playl.sh'>>playl.sh
 
 #end of playl.sh script
 echo "Playlist Created!"
 chmod +x playl.sh
- cp playl.sh $PREFIX/bin/showplay
-chmod +x $PREFIX/bin/showplay
+ #cp playl.sh $PREFIX/bin/showplay
+#chmod +x $PREFIX/bin/showplay
 
      
 ;;
