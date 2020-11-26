31407cabfbb3e38da3b6540fc487fbd09b07a82d
sem sabiduria <semsabiduria@gmail.comgit config --global user.email semsabiduria@gmail.comgit config --global user.email semsabiduria@gmail.com>
2020-11-26 15:49:42 +0800

    Fix

--- volumectrl.sh
+++ volumectrl.sh
@@ -1,14 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
 clear
-
+cd 
         termux-volume >genvolume
 		 #get music volume data
 			mvolume=$(jq '.[] | select(.stream=="music")' genvolume)
 			echo "$mvolume">genvolume
-			jq '.volume' volume>curv
-			jq '.max_volume' volume>maxv
+			jq '.volume' genvolume>curv
+			jq '.max_volume' genvolume>maxv
 
-        dialog --rangebox "Termux Volume Controller" 0 0 1 $(<maxv) $(<curv) 2>defaultvol
+        dialog --rangebox "Termux Volume Controller" 0 0 1 $(<maxv) $(<curv)  2>defaultvol
         
        
 
